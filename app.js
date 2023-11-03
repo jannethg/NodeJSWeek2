ff089fbd16ba40777c6fc9db04ea43852b50e0ca
Janneth Gutierrez <jgutier1@gmail.com>
2023-11-03 04:55:41 -0400

    NodeJS Week 3

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+
 
 const mongoose = require ('mongoose');
 //const url = 'mongodb://127.0.0.1:27017/nucampsite';
@@ -39,6 +42,38 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Authentication Middleware, users authenticate before they access to the server.
+//must have request and response as parameters (req, res)
+// if !authHeader -means the user did not enter a username and password yet
+function auth(req,res,next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if(!authHeader) {
+      const err = new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');  // this lets the client know that the server is requesting authentication
+      err.status = 401;
+      return next(err);
+  }
+    //Buffer global class in Node, from is a static method of buffer, the split and to method belong to vanilla javascript, not node
+    //this will take the username /pwd from the header and it will extract the
+    //authentication from it and put them both into the auth array as the first and second items. 
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    //this will grab the username and password from auth array
+    const user = auth[0];
+    const pass = auth[1];
+    // simple if check if user= 'admin' and password = password
+    if (user === 'admin' && pass === 'password') {
+        return next(); // authorized or access granted
+    } else {
+        const err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');      
+        err.status = 401;
+        return next(err);
+    }
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
