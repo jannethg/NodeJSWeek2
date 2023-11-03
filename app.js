14487818ef47bac763523051c6eb5c4c65b63009
Janneth Gutierrez <jgutier1@gmail.com>
2023-11-03 13:28:15 -0400

    Week 3 - 4. Exercise: Express Sessions Part 2

--- app.js
+++ app.js
@@ -62,34 +62,19 @@ app.use(session({
 
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
 function auth(req, res, next) {
   console.log(req.session);
-  //replace the signedCookies with session since we don't use the parser anymore.
-  if (!req.session.user) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
 
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-          //res.cookie('user', 'admin', {signed: true});  
-          //req.session will be handle by Express session middleware 
-          req.session.user ='admin';
-          return next(); // authorized
-      } else {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
+  if (!req.session.user) {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
   } else {
-      if (req.session.user === 'admin') { //replace signedCookies with session with will be handle by Express session middleware 
+      if (req.session.user === 'authenticated') {
           return next();
       } else {
           const err = new Error('You are not authenticated!');
@@ -98,13 +83,10 @@ function auth(req, res, next) {
       }
   }
 }
-
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
